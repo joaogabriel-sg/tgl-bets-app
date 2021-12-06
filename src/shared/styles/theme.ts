4f3d2a59073147f00b08f37ab933e2f916058666
João Gabriel <main.joaogabrielsg@gmail.com>
2021-12-06 10:14:17 -0300

    feat(theme): create theme and load fonts

---
+++ src/shared/styles/theme.ts
@@ -0,0 +1,22 @@
+export const theme = {
+  fonts: {
+    primary_400: "Roboto_400Regular",
+    primary_700: "Roboto_700Bold",
+  },
+  colors: {
+    background_primary: "#f7f7f7",
+    background_secondary: "#f4f4f4",
+    primary: "#b5c401",
+    shape_light: "#ffffff",
+    shape_medium: "#adc0c4",
+    shape_dark: "#191919",
+    success: "#27C383",
+    error: "#ff7675",
+    border_light: "#ebebeb",
+    border_dark: "#dddddd",
+    text_100: "#c1c1c1",
+    text_500: "#9d9d9d",
+    text_600: "#868686",
+    text_700: "#707070",
+  },
+};
