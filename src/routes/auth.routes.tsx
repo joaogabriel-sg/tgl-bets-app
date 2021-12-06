b5f68099db0477ca1fe11774deeb37190e766e80
João Gabriel <main.joaogabrielsg@gmail.com>
2021-12-06 13:30:20 -0300

    feat: add app initial routes
    - Add app initial routes
    - Add auth routes
    - Add routes param list types

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Authentication, Registration, ResetPassword } from "../screens";
+
+import { RootStackParamList } from "./types";
+
+const AuthStack = createNativeStackNavigator<RootStackParamList>();
+
+export function AuthRoutes() {
+  return (
+    <AuthStack.Navigator screenOptions={{ headerShown: false }}>
+      <AuthStack.Screen name="Authentication" component={Authentication} />
+      <AuthStack.Screen name="Registration" component={Registration} />
+      <AuthStack.Screen name="ResetPassword" component={ResetPassword} />
+    </AuthStack.Navigator>
+  );
+}
---
+++ src/routes/types.ts
@@ -0,0 +1,7 @@
+type AuthStackParamList = {
+  Authentication: undefined;
+  Registration: undefined;
+  ResetPassword: undefined;
+};
+
+export type RootStackParamList = AuthStackParamList;
