5b1ccd479481a6ff6ff5c2714580bc9914fa0456
João Gabriel <main.joaogabrielsg@gmail.com>
2021-12-07 16:14:26 -0300

    feat: change to app routes if has a valid user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
+
+import { useReduxSelector } from "../shared/hooks";
+
+import { selectUser } from "../store/slices/auth/selectors";
 
 export function Routes() {
+  const user = useReduxSelector(selectUser);
+
   return (
     <NavigationContainer>
-      <AuthRoutes />
+      {user ? <AppRoutes /> : <AuthRoutes />}
     </NavigationContainer>
   );
 }
