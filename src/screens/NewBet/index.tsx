8d4cb80a915e6c42726b0b097f28395b0dfcd1ba
João Gabriel <main.joaogabrielsg@gmail.com>
2021-12-08 17:25:05 -0300

    feat(layout): create new bet screen
    - Create NewBet screen
    - Show dynamic game data like type, description and numbers
    - Add functionalities to complete and clear game

--- src/screens/NewBet/index.tsx
+++ src/screens/NewBet/index.tsx
@@ -1,11 +1,177 @@
-import React from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { Alert } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
+
+import { AppHeader, Footer, Loading } from "../../components";
+
+import { useReduxDispatch, useReduxSelector } from "../../shared/hooks";
+import { sortArray } from "../../shared/utils";
+
+import { fetchGames } from "../../store/slices/games/actions";
+import { selectGames } from "../../store/slices/games/selectors";
+import { ITypeOfGames } from "../../store/slices/games";
 
 import * as S from "./styles";
 
 export function NewBet() {
+  const games = useReduxSelector(selectGames);
+  const dispatch = useReduxDispatch();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedGame, setSelectedGame] = useState<ITypeOfGames | null>(null);
+  const [selectedNumbers, setSelectedNumbers] = useState<number[]>([]);
+
+  const gameNumbers = useMemo(() => {
+    if (!selectedGame) return [];
+    return Array.from({ length: selectedGame.range }).map(
+      (_, index) => index + 1
+    );
+  }, [selectedGame]);
+
+  const fetchGamesInApi = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      await dispatch(fetchGames());
+    } catch (err: any) {
+      Alert.alert(err.message, "", [{ text: "Okay" }]);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [dispatch]);
+
+  function handleSelectGameById(id: number) {
+    const newSelectedGame = games.find((game) => game.id === id)!;
+    setSelectedGame(newSelectedGame);
+    setSelectedNumbers([]);
+  }
+
+  function handleToggleNumberSelection(number: number) {
+    setSelectedNumbers((prevSelectedNumbers) => {
+      if (prevSelectedNumbers.includes(number)) {
+        const filteredNumbers = prevSelectedNumbers.filter(
+          (prevSelectedNumber) => prevSelectedNumber !== number
+        );
+        return sortArray(filteredNumbers);
+      }
+
+      if (selectedGame && prevSelectedNumbers.length < selectedGame.max_number)
+        return sortArray([...prevSelectedNumbers, number]);
+
+      return prevSelectedNumbers;
+    });
+  }
+
+  function handleAddToCart() {}
+
+  function handleCompleteGame() {
+    const newNumbers: number[] = [...selectedNumbers];
+
+    if (!selectedGame) return;
+
+    while (newNumbers.length < selectedGame.max_number) {
+      const newNumber = Math.floor(Math.random() * selectedGame.range) + 1;
+
+      if (!newNumbers.includes(newNumber)) newNumbers.push(newNumber);
+    }
+
+    setSelectedNumbers(sortArray(newNumbers));
+  }
+
+  function handleClearGame() {
+    setSelectedNumbers([]);
+  }
+
+  useEffect(() => {
+    if (games.length > 0) setSelectedGame(games[0]);
+  }, [games]);
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGamesInApi();
+    }, [fetchGamesInApi])
+  );
+
   return (
     <S.Container>
-      <S.Title>New Bet</S.Title>
+      <AppHeader />
+
+      {isLoading && <Loading />}
+
+      {!!selectedGame && (
+        <>
+          <S.ScreenTitleContainer>
+            <S.ScreenTitle>
+              New Bet <S.TypeOfGame>for {selectedGame.type}</S.TypeOfGame>
+            </S.ScreenTitle>
+          </S.ScreenTitleContainer>
+
+          <S.Content>
+            <S.SectionTitle>Choose a game</S.SectionTitle>
+            <S.Games
+              data={games}
+              keyExtractor={(item) => String(item.id)}
+              renderItem={({ item }) => (
+                <S.GameButton
+                  active={selectedGame.id === item.id}
+                  color={item.color}
+                  onPress={() => handleSelectGameById(item.id)}
+                >
+                  <S.GameButtonTitle
+                    active={selectedGame.id === item.id}
+                    color={item.color}
+                  >
+                    {item.type}
+                  </S.GameButtonTitle>
+                </S.GameButton>
+              )}
+            />
+
+            <S.SectionTitle>Fill your bet</S.SectionTitle>
+            <S.GameDescription>{selectedGame.description}</S.GameDescription>
+            <S.Numbers>
+              {gameNumbers.map((gameNumber) => (
+                <S.NumberButton
+                  key={gameNumber}
+                  active={!!selectedNumbers.includes(gameNumber)}
+                  color={selectedGame.color}
+                  onPress={() => handleToggleNumberSelection(gameNumber)}
+                >
+                  <S.NumberText>
+                    {`${gameNumber}`.padStart(2, "0")}
+                  </S.NumberText>
+                </S.NumberButton>
+              ))}
+            </S.Numbers>
+
+            <S.ControlButtonsContainer>
+              <S.ControlButtonsRow>
+                <S.ControlButton onPress={handleAddToCart}>
+                  <S.Icon name="cart-outline" />
+                  <S.ControlButtonText>Add to Cart</S.ControlButtonText>
+                </S.ControlButton>
+              </S.ControlButtonsRow>
+
+              <S.ControlButtonsRow>
+                <S.ControlButton isOutlined onPress={handleCompleteGame}>
+                  <S.ControlButtonText isOutlined>
+                    Complete game
+                  </S.ControlButtonText>
+                </S.ControlButton>
+              </S.ControlButtonsRow>
+
+              <S.ControlButtonsRow>
+                <S.ControlButton isOutlined onPress={handleClearGame}>
+                  <S.ControlButtonText isOutlined>
+                    Clear game
+                  </S.ControlButtonText>
+                </S.ControlButton>
+              </S.ControlButtonsRow>
+            </S.ControlButtonsContainer>
+          </S.Content>
+        </>
+      )}
+
+      <Footer />
     </S.Container>
   );
 }
--- src/screens/NewBet/styles.ts
+++ src/screens/NewBet/styles.ts
@@ -1,9 +1,192 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { FlatList, FlatListProps } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import { ITypeOfGames } from "../../store/slices/games";
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
 `;
 
-export const Title = styled.Text``;
+export const ScreenTitleContainer = styled.View`
+  padding: ${RFValue(30)}px ${RFValue(16)}px;
+`;
+
+export const ScreenTitle = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.primary_700_italic};
+    font-size: ${RFValue(24)}px;
+    color: ${theme.colors.text_700};
+    text-transform: uppercase;
+  `}
+`;
+
+export const TypeOfGame = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.primary_400_italic};
+    font-weight: normal;
+    font-size: ${RFValue(24)}px;
+    color: ${theme.colors.text_700};
+  `}
+`;
+
+export const Content = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingHorizontal: RFValue(16),
+  },
+})``;
+
+export const SectionTitle = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.primary_700_italic};
+    font-size: ${RFValue(17)}px;
+    color: ${theme.colors.text_600};
+  `}
+`;
+
+export const Games = styled(
+  FlatList as new (props: FlatListProps<ITypeOfGames>) => FlatList<ITypeOfGames>
+).attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+})`
+  margin-top: 20px;
+  margin-bottom: 36px;
+`;
+
+interface GameButtonProps {
+  active: boolean;
+  color: string;
+}
+
+export const GameButton = styled.TouchableOpacity<GameButtonProps>`
+  ${({ active, color }) => css`
+    background: ${active ? color : "transparent"};
+    min-width: ${RFValue(120)}px;
+    height: ${RFValue(34)}px;
+
+    border-width: ${RFValue(2)}px;
+    border-color: ${color};
+    border-radius: 9999px;
+
+    margin-right: 16px;
+
+    align-items: center;
+    justify-content: center;
+  `}
+`;
+
+export const GameButtonTitle = styled.Text<GameButtonProps>`
+  ${({ theme, active, color }) => css`
+    font-family: ${theme.fonts.primary_700_italic};
+    font-size: ${RFValue(14)}px;
+    color: ${active ? theme.colors.shape_light : color};
+  `}
+`;
+
+export const GameDescription = styled.Text`
+  ${({ theme }) => css`
+    margin-top: 4px;
+
+    font-family: ${theme.fonts.primary_400_italic};
+    font-size: ${RFValue(17)}px;
+    color: ${theme.colors.text_600};
+    line-height: ${RFValue(24)}px;
+  `}
+`;
+
+export const Numbers = styled.View`
+  width: 100%;
+  padding-top: 28px;
+
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+interface NumberButtonProps {
+  active: boolean;
+  color: string;
+}
+
+export const NumberButton = styled.TouchableOpacity<NumberButtonProps>`
+  ${({ theme, active, color }) => css`
+    background: ${active ? color : theme.colors.shape_medium};
+    width: ${RFValue(64)}px;
+    height: ${RFValue(64)}px;
+    border-radius: 999999px;
+    margin-right: 12px;
+    margin-bottom: 12px;
+
+    align-items: center;
+    justify-content: center;
+  `}
+`;
+
+export const NumberText = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.primary_700_italic};
+    font-size: ${RFValue(20)}px;
+    color: ${theme.colors.shape_light};
+  `}
+`;
+
+export const ControlButtonsContainer = styled.View`
+  padding: 12px 0 32px;
+`;
+
+export const ControlButtonsRow = styled.View`
+  margin-bottom: 12px;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+interface ControlButtonProps {
+  isOutlined?: boolean;
+}
+
+export const ControlButton = styled.TouchableOpacity<ControlButtonProps>`
+  ${({ theme, isOutlined }) => css`
+    background: ${theme.colors.success};
+    min-height: ${RFValue(52)}px;
+    border-width: ${RFValue(1)}px;
+    border-color: transparent;
+    border-radius: 10px;
+
+    flex: 1;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+
+    ${isOutlined &&
+    css`
+      background: transparent;
+      border-color: ${theme.colors.success};
+    `}
+  `}
+`;
+
+export const ControlButtonText = styled.Text<ControlButtonProps>`
+  ${({ theme, isOutlined }) => css`
+    margin-left: 16px;
+
+    font-family: ${theme.fonts.primary_700_regular};
+    font-size: ${RFValue(16)}px;
+    color: ${theme.colors.shape_light};
+
+    ${isOutlined &&
+    css`
+      margin-left: 0;
+      font-family: ${theme.fonts.primary_400_regular};
+      color: ${theme.colors.success};
+    `}
+  `}
+`;
+
+export const Icon = styled(Ionicons)`
+  font-size: ${RFValue(26)}px;
+  color: ${({ theme }) => theme.colors.shape_light};
+`;
