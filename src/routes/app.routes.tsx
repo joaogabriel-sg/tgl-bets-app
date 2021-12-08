99fd8d8c5a4364b02732db712613189d33072d71
João Gabriel <main.joaogabrielsg@gmail.com>
2021-12-08 09:52:02 -0300

    feat: change app stack to app drawer

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,21 +1,48 @@
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useTheme } from "styled-components";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import { Account, Home, NewBet } from "../screens";
 
 import { RootStackParamList } from "./types";
 
-const AppStack = createNativeStackNavigator<RootStackParamList>();
+const AppDrawer = createDrawerNavigator<RootStackParamList>();
 
 export function AppRoutes() {
+  const theme = useTheme();
+
   return (
-    <AppStack.Navigator
-      screenOptions={{ headerShown: false }}
+    <AppDrawer.Navigator
       initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        drawerActiveTintColor: theme.colors.shape_dark,
+        drawerActiveBackgroundColor: theme.colors.shape_light,
+        drawerInactiveTintColor: theme.colors.shape_light,
+        drawerContentStyle: {
+          backgroundColor: theme.colors.primary,
+        },
+        drawerLabelStyle: {
+          fontFamily: theme.fonts.primary_700_italic,
+          fontSize: 16,
+        },
+      }}
     >
-      <AppStack.Screen name="Account" component={Account} />
-      <AppStack.Screen name="Home" component={Home} />
-      <AppStack.Screen name="NewBet" component={NewBet} />
-    </AppStack.Navigator>
+      <AppDrawer.Screen
+        name="Home"
+        component={Home}
+        options={{ title: "My Games" }}
+      />
+      <AppDrawer.Screen
+        name="NewBet"
+        component={NewBet}
+        options={{ title: "New Bet" }}
+      />
+      <AppDrawer.Screen
+        name="Account"
+        component={Account}
+        options={{ title: "My Account" }}
+      />
+    </AppDrawer.Navigator>
   );
 }
