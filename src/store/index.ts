4e536ec777fa45037260a3ee90230e09a9726559
João Gabriel <main.joaogabrielsg@gmail.com>
2021-12-08 12:06:21 -0300

    feat(redux): add games reducers and actions
    - Add games setGames reducer
    - Add games clearGames reducer
    - Add games fetchGames thunk action
    - Add games selectMinCartValue selector
    - Add games selectGames selector

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import { authReducer } from "./slices/auth";
+import { gamesReducer } from "./slices/games";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    games: gamesReducer,
   },
 });
---
+++ src/store/slices/games/index.ts
@@ -0,0 +1,45 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface ITypeOfGames {
+  id: number;
+  type: string;
+  description: string;
+  range: number;
+  price: number;
+  max_number: 15;
+  color: string;
+}
+
+export interface IApiGames {
+  min_cart_value: number;
+  types: ITypeOfGames[];
+}
+
+interface IInitialState {
+  minCartValue: number;
+  games: ITypeOfGames[];
+}
+
+const initialState: IInitialState = {
+  minCartValue: 0,
+  games: [],
+};
+
+const gamesSlice = createSlice({
+  name: "@games",
+  initialState,
+  reducers: {
+    setGames: (state, action: PayloadAction<IApiGames>) => {
+      state.minCartValue = action.payload.min_cart_value;
+      state.games = action.payload.types;
+    },
+    clearGames: (state) => {
+      state.minCartValue = 0;
+      state.games = [];
+    },
+  },
+});
+
+export const { setGames } = gamesSlice.actions;
+
+export const gamesReducer = gamesSlice.reducer;
