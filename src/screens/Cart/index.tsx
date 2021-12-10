8f990facb737af5aebe9b689a5703722361e739a
João Gabriel <main.joaogabrielsg@gmail.com>
2021-12-10 07:45:24 -0300

    feat: add save cart games function

--- src/screens/Cart/index.tsx
+++ src/screens/Cart/index.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { Alert } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { useTheme } from "styled-components";
 import { RFValue } from "react-native-responsive-fontsize";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+
+import { Loading } from "../../components";
 
 import EmptyGamesSvg from "../../shared/assets/empty-games.svg";
+
+import { RootStackParamList } from "../../routes";
 
 import { useReduxDispatch, useReduxSelector } from "../../shared/hooks";
 import { formatCurrencyToBRL } from "../../shared/utils";
@@ -12,28 +18,57 @@ import {
   selectCartGames,
   selectCartTotalValue,
 } from "../../store/slices/cart/selectors";
+import { selectMinCartValue } from "../../store/slices/games/selectors";
+import { removeFromCart } from "../../store/slices/cart";
+import { saveNewBet } from "../../store/slices/cart/actions";
 
 import * as S from "./styles";
-import { removeFromCart } from "../../store/slices/cart";
 
 interface Props {
   handleCloseCart: () => void;
 }
 
 export function Cart({ handleCloseCart }: Props) {
+  const [isLoading, setIsLoading] = useState(false);
+
   const cartGames = useReduxSelector(selectCartGames);
   const cartTotalValue = useReduxSelector(selectCartTotalValue);
+  const minCartValue = useReduxSelector(selectMinCartValue);
 
   const dispatch = useReduxDispatch();
 
   const theme = useTheme();
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
   function handleRemoveCartGame(id: string) {
     dispatch(removeFromCart({ id }));
   }
 
+  async function handleSaveCartGames() {
+    try {
+      setIsLoading(true);
+
+      if (cartTotalValue < minCartValue) return;
+
+      const newBet = {
+        games: cartGames.map((cartGame) => ({
+          id: cartGame.gameId,
+          numbers: cartGame.numbers,
+        })),
+      };
+
+      await dispatch(saveNewBet(newBet));
+      navigation.navigate("Home");
+    } catch (err: any) {
+      Alert.alert(err.message, "", [{ text: "Okay" }]);
+      setIsLoading(false);
+    }
+  }
+
   return (
     <S.Container>
+      {isLoading && <Loading />}
+
       <S.Content>
         <S.TitleContainer>
           <S.Title>Cart</S.Title>
@@ -87,7 +122,7 @@ export function Cart({ handleCloseCart }: Props) {
         </S.TitleContainer>
       </S.Content>
 
-      <S.SaveButton>
+      <S.SaveButton onPress={handleSaveCartGames}>
         <S.SaveButtonText>
           Save{" "}
           <Feather name="arrow-right" size={35} color={theme.colors.success} />
