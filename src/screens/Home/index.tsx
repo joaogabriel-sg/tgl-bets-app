c99312dc3cf10a72dae1dfee4be6256a93638df2
João Gabriel <main.joaogabrielsg@gmail.com>
2021-12-10 08:31:29 -0300

    feat: show user bets
    - Dispatch fetchUserBets to fetch the bets
    - Show user bets in the list

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { Alert, FlatList, Text } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { RFValue } from "react-native-responsive-fontsize";
 import { useTheme } from "styled-components";
-import { NavigationProp, useNavigation } from "@react-navigation/native";
+import {
+  NavigationProp,
+  useFocusEffect,
+  useNavigation,
+} from "@react-navigation/native";
 
-import { AppHeader, Footer } from "../../components";
+import { AppHeader, Footer, Loading } from "../../components";
 
 import EmptyGamesSvg from "../../shared/assets/empty-games.svg";
 
 import { RootStackParamList } from "../../routes";
 
+import { useReduxDispatch, useReduxSelector } from "../../shared/hooks";
+
+import { fetchUserBets } from "../../store/slices/bets/actions";
+import { selectBets } from "../../store/slices/bets/selectors";
+
 import * as S from "./styles";
+import { format, parseISO } from "date-fns";
+import { formatCurrencyToBRL } from "../../shared/utils";
 
 export function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const bets = useReduxSelector(selectBets);
+
+  const dispatch = useReduxDispatch();
+
   const theme = useTheme();
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
@@ -20,19 +37,75 @@ export function Home() {
     navigation.navigate("NewBet");
   }
 
+  const fetchUserBetsInApi = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      await dispatch(fetchUserBets());
+    } catch (err: any) {
+      Alert.alert(err.message, "", [{ text: "Okay" }]);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchUserBetsInApi();
+    }, [fetchUserBetsInApi])
+  );
+
   return (
     <S.Container>
+      {isLoading && <Loading />}
+
       <AppHeader />
 
-      <S.EmptyContent>
-        <S.ScreenTitle>Recent Games</S.ScreenTitle>
+      {bets.length === 0 && (
+        <S.EmptyContent>
+          <S.ScreenTitle>Recent Games</S.ScreenTitle>
 
-        <S.InfoLegend>No games</S.InfoLegend>
+          <S.InfoLegend>No games</S.InfoLegend>
 
-        <EmptyGamesSvg height={RFValue(164)} />
+          <EmptyGamesSvg height={RFValue(164)} />
 
-        <S.InfoMessage>You still don't have registered games...</S.InfoMessage>
-      </S.EmptyContent>
+          <S.InfoMessage>
+            You still don't have registered games...
+          </S.InfoMessage>
+        </S.EmptyContent>
+      )}
+
+      {bets.length > 0 && (
+        <S.Content>
+          <FlatList
+            data={bets}
+            keyExtractor={(item) => String(item.id)}
+            showsVerticalScrollIndicator={false}
+            style={{ width: "100%" }}
+            renderItem={({ item }) => {
+              const formattedNumbers = item.choosen_numbers
+                .split(",")
+                .map((number) => `${number}`.padStart(2, "0"))
+                .join(", ");
+
+              return (
+                <S.BetWrapper>
+                  <S.DetailLeft color={item.type.color} />
+                  <S.BetDescription>
+                    <S.BetNumbers>{formattedNumbers}</S.BetNumbers>
+                    <S.BetDateAndPrice>
+                      {format(parseISO(item.created_at), "dd/MM/yyyy")}{" "}
+                      {formatCurrencyToBRL(item.price)}
+                    </S.BetDateAndPrice>
+                    <S.BetType color={item.type.color}>
+                      {item.type.type}
+                    </S.BetType>
+                  </S.BetDescription>
+                </S.BetWrapper>
+              );
+            }}
+          />
+        </S.Content>
+      )}
 
       <S.NewBetButton onPress={handleNavigateToNewBet}>
         <S.NewBetText>New Bet</S.NewBetText>
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,6 +2,10 @@ import styled, { css } from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
 import { RectButton } from "react-native-gesture-handler";
 
+interface BetCardProps {
+  color: string;
+}
+
 export const Container = styled.View`
   flex: 1;
 `;
@@ -50,6 +54,49 @@ export const Content = styled.View`
 
   flex: 1;
   align-items: center;
+`;
+
+export const BetWrapper = styled.View`
+  width: 100%;
+  margin-bottom: 30px;
+  flex-direction: row;
+`;
+
+export const DetailLeft = styled.View<BetCardProps>`
+  background: ${({ color }) => color};
+  width: ${RFValue(6)}px;
+  height: 100%;
+  border-radius: 4px;
+`;
+
+export const BetDescription = styled.View`
+  padding-left: 22px;
+`;
+
+export const BetNumbers = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.primary_700_italic};
+    font-size: ${RFValue(20)}px;
+    color: ${theme.colors.text_600};
+  `}
+`;
+
+export const BetDateAndPrice = styled.Text`
+  ${({ theme }) => css`
+    margin: 16px 0 12px;
+
+    font-family: ${theme.fonts.primary_400_regular};
+    font-size: ${RFValue(17)}px;
+    color: ${theme.colors.text_600};
+  `}
+`;
+
+export const BetType = styled.Text<BetCardProps>`
+  ${({ theme, color }) => css`
+    font-family: ${theme.fonts.primary_700_italic};
+    font-size: ${RFValue(20)}px;
+    color: ${color};
+  `}
 `;
 
 export const NewBetButton = styled(RectButton)`
