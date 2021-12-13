fe31d5d2b0859b1722bfbb3e45b680838f963b80
João Gabriel <main.joaogabrielsg@gmail.com>
2021-12-13 15:18:22 -0300

    feat: create account screen layout
    - Create Account screen layout
    - Validate the account settings form after saving the data

--- src/screens/Account/index.tsx
+++ src/screens/Account/index.tsx
@@ -1,11 +1,95 @@
 import React from "react";
+import { Platform } from "react-native";
+
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import {
+  AppHeader,
+  Button,
+  Footer,
+  InputForm,
+  ScreenTitle,
+} from "../../components";
+
+import { useReduxSelector } from "../../shared/hooks";
+
+import { selectUser } from "../../store/slices/auth/selectors";
 
 import * as S from "./styles";
 
+interface FormData {
+  name: string;
+  email: string;
+}
+
+const schema = yup.object().shape({
+  name: yup.string().required("Name is required!"),
+  email: yup
+    .string()
+    .required("Email is required!")
+    .email("Invalid email address!"),
+});
+
 export function Account() {
+  const user = useReduxSelector(selectUser)!;
+
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  function handleChangeAccountSettings(data: FormData) {}
+
   return (
-    <S.Container>
-      <S.Title>Account</S.Title>
+    <S.Container behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <AppHeader />
+
+      <S.ScreenTitleContainer>
+        <ScreenTitle>Account Settings</ScreenTitle>
+      </S.ScreenTitleContainer>
+
+      <S.Content>
+        <S.FieldWrapper>
+          <S.InputLabel>Name</S.InputLabel>
+          <S.InputWrapper>
+            <InputForm
+              control={control}
+              name="name"
+              error={errors.name ? errors.name.message! : ""}
+              keyboardType="default"
+              defaultValue={user.name}
+            />
+          </S.InputWrapper>
+        </S.FieldWrapper>
+
+        <S.FieldWrapper>
+          <S.InputLabel>E-mail</S.InputLabel>
+          <S.InputWrapper>
+            <InputForm
+              control={control}
+              name="email"
+              error={errors.email ? errors.email.message! : ""}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              defaultValue={user.email}
+            />
+          </S.InputWrapper>
+        </S.FieldWrapper>
+
+        <Button
+          isPrimary
+          title="Save"
+          onPress={handleSubmit(handleChangeAccountSettings)}
+        />
+      </S.Content>
+
+      <Footer />
     </S.Container>
   );
 }
--- src/screens/Account/styles.ts
+++ src/screens/Account/styles.ts
@@ -1,9 +1,45 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { Platform } from "react-native";
 
-export const Container = styled.View`
+export const Container = styled.KeyboardAvoidingView`
   flex: 1;
-  align-items: center;
-  justify-content: center;
 `;
 
-export const Title = styled.Text``;
+export const ScreenTitleContainer = styled.View`
+  padding: ${RFValue(30)}px ${RFValue(16)}px;
+`;
+
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    paddingHorizontal: RFValue(16),
+  },
+})`
+  flex: 1;
+`;
+
+export const FieldWrapper = styled.View`
+  margin-bottom: 24px;
+`;
+
+export const InputLabel = styled.Text`
+  ${({ theme }) => css`
+    margin-bottom: 8px;
+
+    font-family: ${theme.fonts.primary_700_italic};
+    font-size: ${RFValue(20)}px;
+    color: ${theme.colors.text_600};
+  `}
+`;
+
+export const InputWrapper = styled.View`
+  background: ${({ theme }) => theme.colors.background_primary};
+  width: 100%;
+  padding: 12px 16px;
+  border-radius: 4px;
+
+  ${Platform.OS === "android" &&
+  css`
+    elevation: 2;
+  `}
+`;
