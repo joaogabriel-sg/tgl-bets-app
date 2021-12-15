2161e630ff25c014a5549a47bf7916c57fcad281
João Gabriel <main.joaogabrielsg@gmail.com>
2021-12-14 21:10:38 -0300

    chore: rename adding .type.ts

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,5 +1,5 @@
-export * from "./iApiGames";
+export * from "./iApiGames.type";
 export * from "./iAsyncThunkConfig.type";
 export * from "./iBet.type";
-export * from "./iCartGame";
+export * from "./iCartGame.type";
 export * from "./iTypeOfGame.type";
