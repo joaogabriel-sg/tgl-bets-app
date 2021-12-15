9f87235e0215b1b396688b60d7a535a04174bfc7
João Gabriel <main.joaogabrielsg@gmail.com>
2021-12-14 21:11:29 -0300

    feat: add axios interceptors to request with token

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -2,4 +2,5 @@ export * from "./iApiGames.type";
 export * from "./iAsyncThunkConfig.type";
 export * from "./iBet.type";
 export * from "./iCartGame.type";
+export * from "./iToken.type";
 export * from "./iTypeOfGame.type";
